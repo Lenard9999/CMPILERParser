106e6afee9c1ab0a5d99b7562d688c697faf4302
Stephenalexngo <51846517+Stephenalexngo@users.noreply.github.com>
2020-12-26 15:27:41 +0800

    added arraylist for errors to check if theres no syntax errors

--- customfiles/CustomErrorListener.java
+++ customfiles/CustomErrorListener.java
@@ -1,10 +1,14 @@
 package customfiles;
 
+import java.util.ArrayList;
+
 import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.RecognitionException;
 
 public class CustomErrorListener extends BaseErrorListener {
+
+    private ArrayList<String> error_List = new ArrayList<>();
 
     public CustomErrorListener() {
     }
@@ -14,7 +18,12 @@ public class CustomErrorListener extends BaseErrorListener {
             String msg, RecognitionException e) {
 
         String error_Mesaage = msg + " at line " + line + " ,position " + charPositionInLine;
+        error_List.add(error_Mesaage);
 
         System.out.println(error_Mesaage);
+    }
+
+    public int getErrorList(){
+        return this.error_List.size();
     }
 }
